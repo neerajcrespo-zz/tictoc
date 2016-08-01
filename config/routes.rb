5f700a2fdc0b8aca05f63cc8756efc9ccaad692f
neerajcrespo <neerajcrespo@gmail.com>
2016-08-02 01:25:02 +0530

    adding routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   resources :homes
+  get 'handle_move' => 'application#handle_move'
+  root "homes#index"
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
